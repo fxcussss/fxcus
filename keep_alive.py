5f9dce4d89fc85407ea6dd0ad6908873a7e7ccc2
fxcussss <gooroovadoodiyan12@gmail.com>
2025-04-13 15:11:55 +0000

    kok

--- keep_alive.py
+++ keep_alive.py
@@ -2,14 +2,14 @@ from flask import Flask
 from threading import Thread
 import os
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is alive!"
+    return "I'm alive!", 200
 
 def run():
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))  # Use Render's dynamic PORT
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
